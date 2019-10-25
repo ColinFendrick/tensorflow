1eceeb0dc1fe4ed5183aa2f2dc86842bdf0e8fbb
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-25 17:31:08 -0400

    Import and show the mnist data

--- tensorflow/03-Convolutional-Neural-Networks/mnist-data-basic.py
+++ tensorflow/03-Convolutional-Neural-Networks/mnist-data-basic.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
+plt.imshow(mnist.train.images[1].reshape(28, 28), cmap='gist_gray')
+plt.show()
